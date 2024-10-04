4720b2927e0e6c8002621fcbc63708160cdf6365
XenXheka <xen.xheka@student.tsaam.be>
2024-10-04 12:11:56 +0200

    Versie

---
+++ 3_Oefening.py
@@ -0,0 +1,8 @@
+personen= [
+    {"naam": "jan", "leeftijd": 15},
+    {"naam": "han", "leeftijd": 16},
+    {"naam": "lani", "leeftijd": 17},
+    
+]
+for persoon in personen:
+    print(f"naam: {persoon['naam']}, leeftijd {persoon['leeftijd']}")
